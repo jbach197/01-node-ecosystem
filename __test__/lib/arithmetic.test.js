e35637d456b64377f1f42d937b3635b2b90c7a99
Jennifer Bach <jmbach1@gmail.com>
2018-06-10 18:45:09 -0700

    added linter, travis file and cleaned up errors

--- __test__/lib/arithmetic.test.js
+++ __test__/lib/arithmetic.test.js
@@ -6,9 +6,20 @@ const assert = require('assert');
 
 let message = '';
 
+message = arithmetic.add();
+assert.strictEqual(message, null, 'At least two parameters required');
+
+message = arithmetic.add(1, 'A');
+assert.strictEqual(message, null, 'Both parameters must be numbers');
 
 message = arithmetic.add(1, 2);
 assert.strictEqual(message, 3, 'Does not match');
+
+message = arithmetic.sub();
+assert.strictEqual(message, null, 'At least two parameters required');
+
+message = arithmetic.sub(1, 'A');
+assert.strictEqual(message, null, 'Both parameters must be numbers');
 
 message = arithmetic.sub(1, 2);
 assert.strictEqual(message, 1, 'Does not match');
